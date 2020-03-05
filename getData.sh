629831a1163020a318b5f051b2be0272cc96d338
Rahul Mitra <rmitter@cse.iitb.ac.in>
2020-03-05 08:23:07 +0530

    Update getData.sh

--- getData.sh
+++ getData.sh
@@ -4,7 +4,7 @@ mkdir -p MNIST/raw
 cd MNIST/raw
 wget https://www.dropbox.com/s/or8stru3zxybos0/train-images-idx3-ubyte.gz
 wget https://www.dropbox.com/s/1lef0mqf5j5y6ix/train-labels-idx1-ubyte.gz
-https://www.dropbox.com/s/sxft9o9lvia03xx/t10k-images-idx3-ubyte.gz
-https://www.dropbox.com/s/bylkm3rnqvefbp3/t10k-labels-idx1-ubyte.gz
+wget https://www.dropbox.com/s/sxft9o9lvia03xx/t10k-images-idx3-ubyte.gz
+wget https://www.dropbox.com/s/bylkm3rnqvefbp3/t10k-labels-idx1-ubyte.gz
 cd ../../
 mkdir -p images
